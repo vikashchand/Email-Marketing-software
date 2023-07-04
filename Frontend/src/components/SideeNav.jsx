cafd414b947d360d9e2cc16bf20c64b063cf4d7f
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-07-04 07:39:46 +0530

    diable hams

--- Frontend/src/components/SideeNav.jsx
+++ Frontend/src/components/SideeNav.jsx
@@ -1,67 +1,96 @@
-import React, { useState } from 'react';
+
+
+import React,{useState} from 'react';
 import jwtDecode from 'jwt-decode';
 import { NavLink } from 'react-router-dom';
 import './SideeNav.css'; // Import the CSS file
 import { FaBars } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
 
-import { HiUserGroup } from 'react-icons/hi';
-import { MdAdminPanelSettings } from 'react-icons/md';
+import {HiUserGroup} from 'react-icons/hi';
+import{MdAdminPanelSettings} from 'react-icons/md';
 
 import { AiFillHome } from 'react-icons/ai';
-import { FiLogOut } from 'react-icons/fi';
-import { SiAdguard } from 'react-icons/si';
+import { FiLogOut} from 'react-icons/fi'
+import {SiAdguard} from 'react-icons/si';
 
 const SideeNav = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+
+
+ // const navigate = useNavigate();
+ const [isNavOpen, setIsNavOpen] = useState(false);
   const token = localStorage.getItem('userInfo');
   let decodedToken = jwtDecode(token);
   const role = decodedToken.data.is_admin;
+  console.log(role, 'gg');
+
+
 
   const handleLogout = () => {
     localStorage.removeItem('userInfo');
     window.location.href = '/login';
   };
-
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
   return (
-    <div
-      className={`sidenav-container ${
-        isNavOpen ? 'expanded' : 'minimized'
-      } ${!isNavOpen ? 'small-screen' : ''}`}
-    >
-      <div className="menu">
-        <FaBars className="hamburger-icon" onClick={toggleNav} />
-      </div>
-      <NavLink to={'/home/LandingPage'} className="active-link">
-        {isNavOpen ? 'Home' : <AiFillHome />}
-      </NavLink>
-      <NavLink to={'/home/Customers'}>
-        {isNavOpen ? 'Customer Details' : <HiUserGroup />}
-      </NavLink>
-
-      {role == 1 && (
-        <>
-          <NavLink to={'/home/task'}>
-            {isNavOpen ? 'Manage templates' : <FaEnvelope />}
-          </NavLink>
-          <NavLink to="/home/employees">
-            {isNavOpen ? 'Manage Employees' : <MdAdminPanelSettings />}
-          </NavLink>
-          <NavLink to="/home/audit">
-            {isNavOpen ? 'Audit Logs' : <SiAdguard />}
-          </NavLink>
-        </>
-      )}
-
-      <NavLink onClick={handleLogout}>
-        {isNavOpen ? 'LogOut' : <FiLogOut />}
-      </NavLink>
+    <div className={`sidenav-container ${isNavOpen ? 'expanded' : 'minimized'}`}>
+    <div className="menu">
+      <FaBars className="hamburger-icon" onClick={toggleNav} />
     </div>
+    <div className="menu">
+    <FaBars className="hamburger-icons" />
+  </div>
+    <NavLink to={'/home/LandingPage'}  className="active-link">
+    {isNavOpen ? 'Home' : <AiFillHome />}
+      
+    </NavLink>
+    <NavLink to={'/home/Customers'} >
+       
+    {isNavOpen ? 'Customer Details' : <HiUserGroup/>}
+
+
+  </NavLink>
+          
+          {role ==1 && (
+
+            <>
+            <NavLink to={'/home/task'} >
+            {isNavOpen ? 'Manage templates' : <FaEnvelope />}
+            </NavLink>
+            <NavLink to="/home/employees" >
+            {isNavOpen ? 'Manage Employees' : <MdAdminPanelSettings/>}
+            
+            
+            
+            
+            </NavLink>
+            <NavLink to="/home/audit" >
+            {isNavOpen ? 'Audit Logs' : <SiAdguard/>}
+            
+            
+            
+            
+            </NavLink>
+
+            </>
+          )
+          
+        
+        }
+
+       
+          
+          <NavLink onClick={handleLogout}>
+          {isNavOpen ? 'LogOut' : <FiLogOut />}
+        </NavLink>
+
+
+        </div>
   );
 };
 
 export default SideeNav;
+
+
